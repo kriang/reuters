03a61d3830e48b5b0b25eb16c960601e36566878
kriang <kriang@akagu.co>
2018-02-21 18:27:00 +0700

    Updated Header components with a search field, settings and the accounts menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,27 @@ import { NavLink } from 'react-router-dom';
 
 const Header = () => (
     <header>
-        <h1>STOCKIE</h1>
-        <form>
-            <input type="text" placeholder="Search Stocks or Portfolio or News" className="input-search" />
-        </form>
-        <NavLink to="/settings" activeClassName="is-active">Settings</NavLink>
-        <NavLink to="/accounts" activeClassName="is-active">Account</NavLink>
+        <div className="container">
+            <div className="row">
+                
+                <div className="col-xs-8 col-md-4 no-padding">
+                    <div className="input-group header-search">
+                        <input type="text" className="form-control" placeholder="Search" />
+                        <span className="input-group-btn">
+                            <button className="btn btn-default button-search" type="button"><img src="images/icons/magnifying-glass.svg" width="25px;" alt="" /></button>
+                        </span>
+                    </div>{ /* input-group */ }
+                </div>
+                <div className="col-xs-2 col-md-8 pull-right">
+                    <ul className="nav nav-pills pull-right hidden-xs">
+                        <li role="presentation" className="mr-2"><a href="#"><img src="images/icons/cog.svg" width="25px;" alt="" /> Settings</a></li>
+                        <li role="presentation"><a href="#"><img src="images/icons/user.svg" width="25px;" alt="" /> Account</a></li>
+                    </ul>
+                    
+                </div>
+            </div>
+            
+        </div>
     </header>
 );
 
