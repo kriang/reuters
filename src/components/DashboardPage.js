4c9ef069e2ac43526855d2ed684f1c92e494882d
kriang <kriang@akagu.co>
2018-02-21 23:25:26 +0700

    Added MarketPerformance and MyPorfolio to DashboardPage

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -48,6 +48,43 @@ const DashboardPage = () => {
 
                                 </div>
 
+
+                                <div className="col-xs-12 col-md-4">
+                                    <div className="row">
+                                        <div className="card w-100 mb-3">
+                                            
+                                            <div className="card-body">
+                                                <h6 className="card-subtitle mb-2 text-muted">MARKET PERFORMANCE</h6>
+                                                <h2 className="primary-text-color">7,143.62</h2>
+                                                <h5><img src="./images/icons/chevron-small-up.svg" width="35" /> 130.10 (1.86%)</h5>
+                                                <hr className="small" />
+                                                <div className="row">
+                                                    <div className="col-xs-4 mr-3"><small><strong>OPEN</strong>&nbsp;<span className="text-muted">6979.24</span></small></div>
+                                                    <div className="col-xs-4 mr-3"><small><strong>HIGH</strong>&nbsp;<span className="text-muted">7152.05</span></small></div>
+                                                    <div className="col-xs-4"><small><strong>LOW</strong>&nbsp;<span className="text-muted">6977.07</span></small></div>
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                    <div className="row">
+                                        <div className="card w-100">
+                                            
+                                            <div className="card-body">
+                                                <h6 className="card-subtitle mb-2 text-muted">MY PORTFOLIO</h6>
+                                                <h2 className="primary-text-color">$239,123.31</h2>
+                                                <h5><img src="./images/icons/chevron-small-up.svg" width="35" /> 5,342.30 (4.91%)</h5>
+                                                <hr className="small" />
+                                                <div className="row wrapper-chart">
+                                                    <canvas id="myChart" width="400" height="250"></canvas>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    
+                                </div>
+
+
                             </div>
 
 
